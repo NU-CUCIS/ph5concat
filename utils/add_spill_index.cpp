5bbb57d2a7421de2911609c91c988c590943f55e
wkliao <wkliao@users.noreply.github.com>
2021-06-08 00:06:43 -0500

    Use fill mode to populate the new dataset in group /spill

--- utils/add_spill_index.cpp
+++ utils/add_spill_index.cpp
@@ -8,9 +8,10 @@
 #include <config.h>
 #endif
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> /* strcmp(), strdup() */
+#include <string.h> /* strdup() */
 #include <unistd.h> /* getopt() */
 #include <assert.h> /* assert() */
 
@@ -23,20 +24,15 @@ using namespace std;
 
 static int verbose;
 
-#define RETURN_ERROR(func_name, obj_name) {				\
-	printf("Error in %s line %d: calling %s for object %s\n",__FILE__,__LINE__,func_name,obj_name); \
-	return -1;							\
-    }
-
-#define HANDLE_ERROR(msg) {				\
-    printf("Error at line %d: %s\n",__LINE__, msg);	\
-    err_exit = -1;					\
-    goto fn_exit;					\
+#define RETURN_ERROR(func_name, obj_name) {   \
+    cout<<"Error at line "<<__LINE__<<" calling "<<func_name<<" for object "<<obj_name<<endl; \
+    return -1;                                \
 }
 
-#define CALLBACK_ERROR(func_name, dset_name) {			\
-    printf("Error in %s line %d: calling %s for group %s dataset %s\n",__FILE__,__LINE__,func_name, grp_name, dset_name); \
-    return -1;								\
+#define HANDLE_ERROR(msg) {                                   \
+    cout<<"Error at line "<<__LINE__<<" failed: "<<msg<<endl; \
+    err_exit = -1;                                            \
+    goto fn_exit;                                             \
 }
 
 /* string-based basename */
@@ -84,7 +80,7 @@ int incr_raw_data_chunk_cache(hid_t fapl_id)
 
     err = H5Pset_cache(fapl_id, mdc_nelmts, rdcc_nslots, rdcc_nbytes, w0);
     if (err < 0) HANDLE_ERROR("H5Pset_cache");
- fn_exit:
+fn_exit:
     return err_exit;
 }
 
@@ -108,88 +104,21 @@ int check_h5_objects(const char *filename, hid_t fid)
     if (howmany > 1) printf("open objects:\n");
 
     for (ii=0; ii<howmany; ii++) {
-	string type_name="";
-	ot = H5Iget_type(objs[ii]);
-	if (ot == H5I_FILE)      continue; /*  type_name = "H5I_FILE"; */
-	else if (ot == H5I_GROUP)     type_name = "H5I_GROUP";
-	else if (ot == H5I_DATATYPE)  type_name = "H5I_DATATYPE";
-	else if (ot == H5I_DATASPACE) type_name = "H5I_DATASPACE";
-	else if (ot == H5I_DATASET)   type_name = "H5I_DATASET";
-	else if (ot == H5I_ATTR)      type_name = "H5I_ATTR";
-	H5Iget_name(objs[ii], obj_name, 1024);
-	printf("%s %4zd: type %s, name %s\n",
-	       filename, ii, type_name.c_str(), obj_name);
+        string type_name="";
+        ot = H5Iget_type(objs[ii]);
+        if (ot == H5I_FILE)      continue; /*  type_name = "H5I_FILE"; */
+        else if (ot == H5I_GROUP)     type_name = "H5I_GROUP";
+        else if (ot == H5I_DATATYPE)  type_name = "H5I_DATATYPE";
+        else if (ot == H5I_DATASPACE) type_name = "H5I_DATASPACE";
+        else if (ot == H5I_DATASET)   type_name = "H5I_DATASET";
+        else if (ot == H5I_ATTR)      type_name = "H5I_ATTR";
+        H5Iget_name(objs[ii], obj_name, 1024);
+        printf("Still opened in file %s %4zd: type %s, name %s\n",
+               filename, ii, type_name.c_str(), obj_name);
     }
     free(objs);
     return howmany;
 }
-
-/*----< allocate_buffer() >--------------------------------------------------*/
-/* support adding additional index values of integral type */
-int 
-allocate_buffer(void *&buffer, const size_t & len, const hid_t & h5type)
-{
-    htri_t equality = H5Tequal(h5type, H5T_INTEGER);
-    
-    if (equality > 0) { /* h5type is integral */
-	buffer = malloc(len * H5Tget_size(h5type));
-	return 0;
-    }
-    else if (equality == 0) { /* h5type is not integral */
-	printf("Unsupported datatype\n");
-	return -1;
-    }
-    else { /* equality check failed */
-	RETURN_ERROR("H5Tequal", "h5type");
-	/* returns -1 */
-    }
-    
-    /*
-    switch (H5Tget_class(h5type)) {
-    case H5T_NATIVE_CHAR:
-	buffer = (char*          ) malloc(len * sizeof(char          ));
-	break;			 				     
-    case H5T_NATIVE_SCHAR:	 				     
-	buffer = (signed char*   ) malloc(len * sizeof(signed char   ));
-	break;			 				     
-    case H5T_NATIVE_UCHAR:	 				     
-	buffer = (unsigned char* ) malloc(len * sizeof(unsigned char ));
-	break;			 				     
-    case H5T_NATIVE_SHORT:	 				     
-	buffer = (short*         ) malloc(len * sizeof(short         ));
-	break;
-    case H5T_NATIVE_USHORT:
-	buffer = (unsigned short*) malloc(len * sizeof(unsigned short));
-	break;
-    case H5T_NATIVE_INT:
-	buffer = (int*           ) malloc(len * sizeof(int           ));
-	break;
-    case H5T_NATIVE_UINT:
-        buffer = (unsigned int*  ) malloc(len * sizeof(unsigned int  ));
-	break;
-    case H5T_NATIVE_HSIZE:
-	buffer = (hsize_t*       ) malloc(len * sizeof(hsize_t       ));
-	break;
-    case H5T_NATIVE_HSSIZE:
-	buffer = (hssize_t*      ) malloc(len * sizeof(hssize_t      ));
-	break;
-    case H5T_NATIVE_HERR:
-	buffer = (herr_t*        ) malloc(len * sizeof(herr_t        ));
-	break;
-    default:
-	char dtype_name[1024];
-	herr_t err;
-	err = H5Tenum_nameof(h5type, dtype_name, 1024);
-	if(err < 0) RETURN_ERROR("H5Tenum_nameof", "h5type");
-	else {
-	    printf("Error: %s not supported\n", dtype_name);
-	    goto fn_exit;
-	}
-    }
-    */
-}
-
-
 
 /*----< usage() >------------------------------------------------------------*/
 static void
@@ -198,73 +127,71 @@ usage(char *progname)
 #define USAGE   "\
   [-h]          print this command usage message\n\
   [-v]          verbose mode (default: off)\n\
-  -s source     full path to dataset who's value will be injected into the /spill group\n\
-                multiple values allowed (required)\n\
+  [-n]          dry-run mode (default: off)\n\
+  -s src_path   full path of dataset who's first element's value will be used\n\
+                to populate the new dataset in group '/spill' (required)\n\
   file_name     input/output HDF5 file name (required)\n\n\
-  This utility program adds a new dataset in the /spill group of the input file.\n\
-  The new dataset should be single-valued, is determined by the source dataset,\n\
-  and is intended to be used as an additional index use for partition key generation.\n\
-  Requirements for the HDF5 file:\n\
-    1. must contain group /spill\n\
+  This utility program adds a new dataset in group '/spill' of the input\n\
+  file. Argument 'src_path' should be in the form of '/group/dset'. The new\n\
+  dataset to be created will be '/spill/dset', whose contents will be\n\
+  single-valued, populated from the first element of '/group/dset'. This new\n\
+  dataset is intended to be used as an additional index dataset for\n\
+  generating partition key datasets. Requirements for the HDF5 file:\n\
+    1. must contain group '/spill'\n\
     2. contains multiple groups at root level\n\
     3. each group may contain multiple 2D datasets\n\
     4. all datasets in the same group share the 1st dimension size\n\
-    5. each group must contain the source datasets\n\
-    6. the second dimension size of the source datasets must be 1\n\
-    7. data type of the source dataset(s) must be H5T_STD_U32LE\n\
+    5. dataset src_path must exist, except for the one in group '/spill'\n\
+    6. the second dimension size of the dataset 'dset' must be 1\n\
   *ph5concat version _PH5CONCAT_VERSION_ of _PH5CONCAT_RELEASE_DATE_\n"
 
-    printf("Usage: %s [-h|-v] -k base_name file_name\n%s\n", progname, USAGE);
+    printf("Usage: %s [-h|-v|-n] -s src_path file_name\n%s\n", progname, USAGE);
 }
-
 
 int main(int argc, char **argv)
 {
-    char msg[1024], *infile=NULL;
-    hid_t file_id=-1, fapl_id=-1;
-    char * source=NULL;
-    int c, err_exit=0;
+    char *infile=NULL, *src_path=NULL, *newsrc_name;
+    int c, err_exit=0, ndims;
+    double inVal;
+    hid_t file_id=-1, fapl_id=-1, dcpl_id, dset, mspace, fspace;
+    hid_t spill_id=-1, space_id=-1, run_id, newsrc_id, dtype;
+    htri_t src_exist;
     herr_t err;
     hsize_t one[2]={1,1}, offs[2]={0,0}, lens[2]={1,1};
-    hid_t dset_id, mem_space_id, file_space_id;
-    int *inbuf, *outbuf;
-    bool dry_run;
-    hid_t spill_id, space_id, run_id, dcpl_id, newsrc_id;
     hsize_t dset_dims[2], maxdims[2];
-    int ndims;
-    
+    bool dry_run=false;
+
     verbose = 0; /* default is quiet */
 
     /* command-line arguments */
     while ((c = getopt(argc, argv, "hvns:")) != -1) {
-	switch(c) {
-	    case 'h': usage(argv[0]);
-		      err_exit = -1;
-		      goto fn_exit;
-	    case 'n': dry_run = true;
-		      break;
-	    case 's': source = strdup(optarg);
-		      break;
-	    case 'v': verbose = 1;
-	              break;
-	    default: break;
-	}
+        switch(c) {
+            case 'n': dry_run = true;
+                      break;
+            case 's': src_path = strdup(optarg);
+                      break;
+            case 'v': verbose = 1;
+                      break;
+            case 'h': usage(argv[0]);
+                      return 0;
+            default:  usage(argv[0]);
+                      return 1;
+        }
     }
     if (argv[optind] == NULL) { /* input file name is mandatory */
-        printf("input file name is missing\n");
+        fprintf(stderr,"Error: input file name is missing\n");
         usage(argv[0]);
         err_exit = -1;
         goto fn_exit;
     }
-    if (source == NULL) { /* source dataset is required */
-	printf("source dataset is required\n");
-	usage(argv[0]);
-	err_exit = -1;
-	goto fn_exit;
+    if (src_path == NULL) { /* src_path dataset is required */
+        fprintf(stderr,"Error: src_path dataset is required\n");
+        usage(argv[0]);
+        err_exit = -1;
+        goto fn_exit;
     }
 
     infile = strdup(argv[optind]);
-
     if (verbose) printf("input file: %s\n", infile);
 
     /* create file access property for read and write */
@@ -274,60 +201,77 @@ int main(int argc, char **argv)
     /* increase cache size for raw data */
     err = incr_raw_data_chunk_cache(fapl_id);
     if (err < 0) HANDLE_ERROR("incr_raw_data_chunk_cache");
-    
+
     if (dry_run)
-	/* read-only mode */
-	file_id = H5Fopen(infile, H5F_ACC_RDONLY, fapl_id);
+        /* read-only mode */
+        file_id = H5Fopen(infile, H5F_ACC_RDONLY, fapl_id);
     else
-	/* read-and-write mode  */
-	file_id = H5Fopen(infile, H5F_ACC_RDWR, fapl_id);
-    if (file_id < 0) {
-        sprintf(msg, "Can't open input file %s\n", infile);
-        HANDLE_ERROR(msg);
+        /* read-and-write mode  */
+        file_id = H5Fopen(infile, H5F_ACC_RDWR, fapl_id);
+    if (file_id < 0)
+        HANDLE_ERROR(string("H5Fopen ")+infile);
+
+    /* check if dataset src_path exists */
+    src_exist = H5Lexists(file_id, src_path, H5P_DEFAULT);
+    if (src_exist < 0) RETURN_ERROR("H5Lexists", src_path)
+    if (src_exist == 0) {
+        fprintf(stderr,"Error: src_path dataset '%s' does not exist\n", src_path);
+        err_exit = -1;
+        goto fn_exit;
     }
 
     /* create memspace */
-    mem_space_id = H5Screate_simple(2, lens, NULL);
-    if (mem_space_id < 0) HANDLE_ERROR("H5Screate_simple");
-    
-    /* open the source dataset */
-    dset_id = H5Dopen(file_id, source, H5P_DEFAULT);
-    if (dset_id < 0) RETURN_ERROR("H5Dopen", source);
+    mspace = H5Screate_simple(2, lens, NULL);
+    if (mspace < 0) HANDLE_ERROR("H5Screate_simple");
 
-    /* allocate buffer for data to be read into */
-    inbuf = new int[1];
+    /* open the src_path dataset */
+    dset = H5Dopen(file_id, src_path, H5P_DEFAULT);
+    if (dset < 0) RETURN_ERROR("H5Dopen", src_path);
 
-    /* set the window */
-    file_space_id = H5Dget_space(dset_id);
-    if (file_space_id < 0) RETURN_ERROR("H5Dget_space", source);
-	        
-    err = H5Sselect_hyperslab(file_space_id, H5S_SELECT_SET, offs, NULL,
-			      one, lens);
-    if (err < 0) RETURN_ERROR("H5Sselect_hyperslab", source);
+    /* get the data type, as the new dataset will be of the same type */
+    dtype = H5Dget_type(dset);
+    if (dtype < 0) HANDLE_ERROR("H5Dget_type")
 
-    /* read data */
-    err = H5Dread(dset_id, H5T_NATIVE_INT, mem_space_id, file_space_id,
-		  H5P_DEFAULT, inbuf);
-    
-    if (err < 0) RETURN_ERROR("H5Dread", source);
-	
-    err = H5Dclose(dset_id);
-    if (err < 0) RETURN_ERROR("H5Dclose", source);
-    H5Sclose(file_space_id);
-
-    if(verbose) { /* print values we retrieved from the source datasets */
-	printf("%s = %d\n", source, *inbuf);
+    /* check if this dataset is of zero size */
+    fspace = H5Dget_space(dset);
+    if (fspace < 0) RETURN_ERROR("H5Dget_space", src_path);
+    ndims = H5Sget_simple_extent_dims(fspace, dset_dims, NULL);
+    if (ndims < 0)
+        HANDLE_ERROR(string("H5Sget_simple_extent_dims ")+src_path);
+    if (dset_dims[0] == 0) {
+        cerr << "Error at line "<<__LINE__<<": zero-sized dataset '"<<src_path<<"'"<<endl;
+        err_exit = -1;
+        goto fn_exit;
     }
-    
+
+    err = H5Sselect_hyperslab(fspace, H5S_SELECT_SET, offs, NULL, one, lens);
+    if (err < 0) RETURN_ERROR("H5Sselect_hyperslab", src_path);
+
+    /* read into an 8-type buffer (of type double) large enough for any type */
+    err = H5Dread(dset, H5T_NATIVE_DOUBLE, mspace, fspace, H5P_DEFAULT, &inVal);
+    if (err < 0) RETURN_ERROR("H5Dread", src_path);
+
+    err = H5Dclose(dset);
+    if (err < 0) RETURN_ERROR("H5Dclose", src_path);
+
+    err = H5Sclose(mspace);
+    if (err < 0) HANDLE_ERROR("H5Sclose")
+
+    err = H5Sclose(fspace);
+    if (err < 0) HANDLE_ERROR("H5Sclose")
+
+    if (verbose) /* print values we retrieved from the src_path datasets */
+        printf("src_path dataset %s = %e\n", src_path, inVal);
+
     /* open spill group */
     spill_id = H5Gopen(file_id, "/spill", H5P_DEFAULT);
     if (spill_id < 0) RETURN_ERROR("H5Gopen", "/spill");
-			
-    /* open run dataset in spill group to determine dataset size*/
+
+    /* open run dataset in spill group to determine dataset size */
     run_id = H5Dopen(spill_id, "run", H5P_DEFAULT);
     if (run_id < 0) RETURN_ERROR("H5Dopen", "run");
 
-    /* Get dimension sizes of run dataset */
+    /* Get dimension sizes of dataset /spill/run */
     space_id = H5Dget_space(run_id);
     if (space_id < 0) RETURN_ERROR("H5Dget_space", "run");
     ndims = H5Sget_simple_extent_dims(space_id, dset_dims, maxdims);
@@ -342,44 +286,56 @@ int main(int argc, char **argv)
     if (err < 0) RETURN_ERROR("H5Dclose", "run");
 
     if (verbose) printf("/spill/run dims (%llu, 1)\n", dset_dims[0]);
-    
-    if (!dry_run) {
-	char * newsrc_name = basename(source);
 
-	/* allocate out buffer */
-	outbuf = (int*) malloc(dset_dims[0] * sizeof(int));
-	if (verbose) printf("Filling /spill/%s (%llu, 1) with %d\n", newsrc_name, dset_dims[0], *inbuf);
+    /* /spill/run should not be of zero-sized */
+    if (dset_dims[0] == 0) RETURN_ERROR("Zero-sized group '/spill'", "run");
 
-	/* fill out buffer with value determined from source */
-	for(auto ib = 0u; ib < dset_dims[0]; ib++) outbuf[ib] = inbuf[0];
-
-	/* create new dataset */
-	newsrc_id = H5Dcreate2(spill_id, newsrc_name, H5T_NATIVE_INT, space_id,
-			       H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
-	if (newsrc_id < 0) RETURN_ERROR("H5Dcreate2", newsrc_name);
-	
-	/* write to new dataset */
-	if (dset_dims[0] > 0) {
-	    err = H5Dwrite(newsrc_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL,
-			   H5P_DEFAULT, outbuf);
-	    if (err < 0) RETURN_ERROR("H5Dwrite", newsrc_name);
-	}
-	
-	/* close dataset */
-	err = H5Dclose(newsrc_id);
-	if (err < 0) RETURN_ERROR("H5Dclose", newsrc_name);
-
+    newsrc_name = basename(src_path);
+    src_exist = H5Lexists(spill_id, newsrc_name, H5P_DEFAULT);
+    if (src_exist < 0) RETURN_ERROR("H5Lexists", newsrc_name)
+    if (src_exist > 0) {
+        fprintf(stderr,"Error: dataset '%s' already exists in group '/spill'\n",
+                newsrc_name);
+        err_exit = -1;
+        goto fn_exit;
     }
 
-    
-    /* close spill group */
-    err = H5Gclose(spill_id);
-    if (err < 0) RETURN_ERROR("H5Gclose", "/spill");
+    if (!dry_run) {
+        /* Fill the entire new dataset with the same value. Data type casting
+         * will be internally triggered by HDF5, if newsrc_name is not of
+         * type double.
+         */
+        err = H5Pset_fill_value(dcpl_id, H5T_NATIVE_DOUBLE, &inVal);
+        if (err < 0) RETURN_ERROR("H5Pset_fill_value", newsrc_name);
 
-    /* release buffers */
-    if (outbuf != NULL) free(outbuf);
-    if (inbuf  != NULL) free(inbuf );
- fn_exit:
+        err = H5Pset_fill_time(dcpl_id, H5D_FILL_TIME_ALLOC);
+        if (err < 0) RETURN_ERROR("H5Pset_fill_time", newsrc_name);
+
+        /* create new dataset */
+        newsrc_id = H5Dcreate2(spill_id, newsrc_name, dtype, space_id,
+                               H5P_DEFAULT, dcpl_id, H5P_DEFAULT);
+        if (newsrc_id < 0) RETURN_ERROR("H5Dcreate2", newsrc_name);
+
+        /* close dataset */
+        err = H5Dclose(newsrc_id);
+        if (err < 0) RETURN_ERROR("H5Dclose", newsrc_name);
+    }
+
+    err = H5Pclose(dcpl_id);
+    if (err < 0) HANDLE_ERROR("H5Pclose");
+    err = H5Tclose(dtype);
+    if (err < 0) HANDLE_ERROR("H5Tclose");
+
+fn_exit:
+    if (space_id != -1) {
+        err = H5Sclose(space_id);
+        if (err < 0) HANDLE_ERROR("H5Sclose")
+    }
+    if (spill_id != -1) {
+        /* close spill group */
+        err = H5Gclose(spill_id);
+        if (err < 0) RETURN_ERROR("H5Gclose", "/spill");
+    }
     if (file_id >= 0) {
         check_h5_objects(infile, file_id);
         err = H5Fclose(file_id);
@@ -389,6 +345,9 @@ int main(int argc, char **argv)
         err = H5Pclose(fapl_id);
         if (err < 0) printf("Error at line %d: H5Pclose\n",__LINE__);
     }
+    if (src_path != NULL) free(src_path);
+    if (infile   != NULL) free(infile);
+
     return (err_exit == -1);
 }
 

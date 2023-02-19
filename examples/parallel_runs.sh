dce47605bc7bb017ec7710b2208ee18d34326605
wkliao <wkliao@users.noreply.github.com>
2023-02-19 16:09:58 -0600

    set env variable TESTMPIRUN

--- examples/parallel_runs.sh
+++ examples/parallel_runs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -l
 #
 # Copyright (C) 2019, Northwestern University and Fermi National Accelerator Laboratory
 # See COPYRIGHT notice in top-level directory.
@@ -6,14 +7,26 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "srcdir=$srcdir"
+# echo "srcdir=$srcdir"
+# echo "TESTMPIRUN=$TESTMPIRUN"
 
 rm -f ./out.h5
-mpiexec -n 2 ../ph5_concat -i ${srcdir}/sample_list.txt -o out.h5
+CMD="${TESTMPIRUN} -n 2 ../ph5_concat -i ${srcdir}/sample_list.txt -o out.h5"
+echo "CMD=$CMD"
+$CMD
+echo "========================================================================"
+echo ""
 
 rm -f ./out.h5
-mpiexec -n 2 ../ph5_concat -i ${srcdir}/sample_list.txt -o out.h5 -k evt
+CMD="${TESTMPIRUN} -n 2 ../ph5_concat -i ${srcdir}/sample_list.txt -o out.h5 -k evt"
+echo "CMD=$CMD"
+$CMD
+echo "========================================================================"
+echo ""
 
 rm -f ./out.h5
-mpiexec -n 1 ../ph5_concat -i ${srcdir}/vl_string.txt -o out.h5 -b 1
+CMD="${TESTMPIRUN} -n 1 ../ph5_concat -i ${srcdir}/vl_string.txt -o out.h5 -b 1"
+echo "CMD=$CMD"
+$CMD
+echo "========================================================================"
 
